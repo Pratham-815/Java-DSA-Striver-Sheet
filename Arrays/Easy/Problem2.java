c78c718d81b741cc4b02d98129d46ccee5d66af0
Pratham Agarwal <prathamagarwal815@gmail.com>
2025-02-22 12:06:46 +0530

    Add solution to find the second largest element in an array

---
+++ Arrays/Easy/Problem2.java
@@ -0,0 +1,21 @@
+package Arrays.Easy;
+
+public class Problem2 {
+    class Solution {
+        public int getSecondLargest(int[] arr) {
+            int largest = arr[0];
+            
+            for(int i: arr){
+                if(largest < i) largest = i;
+            }
+            
+            int second_largest = -1;
+            
+            for(int i: arr){
+                if(second_largest < i && i != largest) second_largest = i;
+            }
+            
+            return second_largest;
+        }
+    }
+}
