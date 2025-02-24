98202d1610e1b09d6728c0ba14d8566e904dde73
Pratham Agarwal <prathamagarwal815@gmail.com>
2025-02-24 18:39:48 +0530

    Add solution to move all zeroes to the end of an array

---
+++ Arrays/Easy/Problem6.java
@@ -0,0 +1,24 @@
+package Arrays.Easy;
+
+// Solution to move all zeroes to the end of Array
+
+public class Problem6{
+    public void moveZeroess(int[] nums){
+        int count = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == 0) count = count + 1;
+        }
+
+        int j = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] != 0){
+                nums[j] = nums[i];
+                j = j + 1;
+            }
+        }
+
+        for(int i = nums.length - count; i<nums.length; i++){
+            numms[i] = 0;
+        }
+    }
+}
